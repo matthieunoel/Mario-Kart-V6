c0b1b8e7cd40ef984863462e781dde102996a3e7
matthieunoel <44287826+matthieunoel@users.noreply.github.com>
2020-05-18 22:19:05 +0200

    Finis mais non testé.

--- SolutionMKV6/Score.cs
+++ SolutionMKV6/Score.cs
@@ -8,9 +8,11 @@ namespace SolutionMKV6
 {
     public class Score
     {
+        private int id;
         private int numCourse;
         private int valeur;
 
+        public int Id { get => id; set => id = value; }
         public int NumCourse { get => numCourse; set => numCourse = value; }
         public int Valeur { get => valeur; set => valeur = value; }
 
@@ -22,6 +24,13 @@ namespace SolutionMKV6
 
         public Score(int numCourse, int score)
         {
+            this.NumCourse = numCourse;
+            this.Valeur = score;
+        }
+
+        public Score(int id, int numCourse, int score)
+        {
+            this.Id = id;
             this.NumCourse = numCourse;
             this.Valeur = score;
         }
